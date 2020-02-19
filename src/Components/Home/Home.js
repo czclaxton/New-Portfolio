776bee3f4ba68d5e93e6e7818466473d69e65987
Connor Claxton <czclaxton@gmail.com>
2020-02-18 18:03:05 -0600

    start on home page

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 
 import { MainContainer, Button, Text } from "./Home.styled";
 import { colors } from "../../Shared_Styles/Style_Variables/colors";
