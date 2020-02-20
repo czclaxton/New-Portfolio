91427a79cf47bf6b3d4b4f852cc0279e3afa00d1
Connor Claxton <czclaxton@gmail.com>
2020-02-19 19:14:43 -0600

    project page finished

--- src/Components/MyWork/MyWork.js
+++ src/Components/MyWork/MyWork.js
@@ -1,61 +1,48 @@
 import React, { useState, useEffect } from "react";
 import DisplayCard from "./Display/DisplayCard";
+import { useMediaQuery } from "react-responsive";
 
 import { Modal } from "styled-react-modal";
 import {
   MainContainer,
   DisplayContainer,
-  FilterButton,
-  FilterButtonWrapper,
-  FilterButtonMenu,
-  Header
+  Header,
+  MainContainerMobile,
+  DisplayContainerMobile,
+  HeaderMobile
 } from "./MyWork.styled";
-import {
-  SubTitle,
-  Paragraph,
-  SmallSubTitle
-} from "../../Shared_Styles/TextStyles";
 import { projects } from "./projects";
 
 const MyWork = () => {
   const [projectList, setProjectList] = useState(projects);
   const [selectedSkill, setSelectedSkill] = useState("All");
 
-  // const skillList = ["All", "JavaScript", "React.js", "Node.js"];
-  // useEffect(() => {
-  //   const filtered = projects.filter(project =>
-  //     project.primarySkills.includes(selectedSkill)
-  //   );
-  //   if (selectedSkill === "All") {
-  //     setProjectList(projects);
-  //   } else {
-  //     setProjectList(filtered);
-  //   }
-  // }, [selectedSkill]);
+  const isMobile = useMediaQuery({
+    query: "(max-width: 800px)"
+  });
 
   return (
-    <MainContainer>
-      <Header>PROJECTS</Header>
-      {/* <FilterButtonMenu>
-        <FilterButtonWrapper>
-          {skillList.map((skill, i) => (
-            <FilterButton
-              className="filter"
-              data-filter={skill}
-              onClick={() => setSelectedSkill(skill)}
-              key={i}
-            >
-              {skill}
-            </FilterButton>
-          ))}
-        </FilterButtonWrapper>
-      </FilterButtonMenu> */}
-      <DisplayContainer>
-        {projectList.map((project, i) => {
-          return <DisplayCard key={i} project={project} />;
-        })}
-      </DisplayContainer>
-    </MainContainer>
+    <>
+      {isMobile ? (
+        <MainContainerMobile>
+          <HeaderMobile>PROJECTS</HeaderMobile>
+          <DisplayContainerMobile>
+            {projectList.map((project, i) => {
+              return <DisplayCard key={i} project={project} />;
+            })}
+          </DisplayContainerMobile>
+        </MainContainerMobile>
+      ) : (
+        <MainContainer>
+          <Header>PROJECTS</Header>
+          <DisplayContainer>
+            {projectList.map((project, i) => {
+              return <DisplayCard key={i} project={project} />;
+            })}
+          </DisplayContainer>
+        </MainContainer>
+      )}
+    </>
   );
 };
 
