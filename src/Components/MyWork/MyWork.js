a7b0960151780f5477b6f13885b653f28ffb4a38
Connor Claxton <czclaxton@gmail.com>
2020-03-25 20:33:45 -0500

    updated image alt tag

--- src/Components/MyWork/MyWork.js
+++ src/Components/MyWork/MyWork.js
@@ -21,35 +21,9 @@ const MyWork = () => {
   const [projectList, setProjectList] = useState(projects);
   const [selectedSkill, setSelectedSkill] = useState("All");
 
-  // const skillList = ["All", "JavaScript", "React.js", "Node.js"];
-  // useEffect(() => {
-  //   const filtered = projects.filter(project =>
-  //     project.primarySkills.includes(selectedSkill)
-  //   );
-  //   if (selectedSkill === "All") {
-  //     setProjectList(projects);
-  //   } else {
-  //     setProjectList(filtered);
-  //   }
-  // }, [selectedSkill]);
-
   return (
     <MainContainer>
       <Header>PROJECTS</Header>
-      {/* <FilterButtonMenu>
-        <FilterButtonWrapper>
-          {skillList.map((skill, i) => (
-            <FilterButton
-              className="filter"
-              data-filter={skill}
-              onClick={() => setSelectedSkill(skill)}
-              key={i}
-            >
-              {skill}
-            </FilterButton>
-          ))}
-        </FilterButtonWrapper>
-      </FilterButtonMenu> */}
       <DisplayContainer>
         {projectList.map((project, i) => {
           return <DisplayCard key={i} project={project} />;
